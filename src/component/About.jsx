84faa929214d0ad5d4430116672e2d726c08965e
santoshCS <scs.gamer39@gmail.com>
2023-10-21 15:19:55 +0530

    addes pictures
    added photo

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -2,20 +2,30 @@ import React from 'react'
 import './About.css';
 import jsonData from '../data/data.json';
 
+
 const About = () => {
 
   return (
     <>
     <div className="container box_area">
-        <div className="career_section"><h1>About</h1></div>
-        <div className="career_section"><h1>About</h1></div>
+        <div className="career_section"><h1>ABOUT</h1>
+        <p className='carnity_txt'>Career Opportunity</p>
+        </div>
+        <div className="career_section_img"><img src={process.env.PUBLIC_URL + '/assets/Rectangle.svg'} alt="Icon" /></div>
     </div>
-
+    <div className="tex_box">
+      <p className="text_head">Empowering Young Minds<br></br>
+The <span>Ultimate Job Solution</span> for Fresher
+        </p>
+        <p className="text_content">
+        We have the infrastructure to address the widespread issue of freshly graduated students needing help getting<br></br> their dream job. Our solution covers the full spectrum, from helping people find work to imparting the skills they<br></br> need to succeed in the workplace and assisting them in navigating the hiring process.
+        </p>
+      </div>
     <div className="container">
       {jsonData.first.map(item => (
         <div key={item.id} className="item">
           <div className="icon">
-            <img src={item.icon} alt="Icon" />
+            <img src={item.icon} alt="Icon" /> 
           <div className="title">{item.title}</div>
           </div>
           <div className="text">{item.text}</div>
